8a3f6d892d580b9aeba0b238a7c1345110ef5cab
JonasFranco <minhacontajonas@gmail.com>
2024-01-23 23:43:15 -0300

    Iniciando a criação do home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,10 @@
 export function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <strong>Encotre o café perfeito para qualquer hora do dia</strong>
+      <p>
+        Com o caffee Delivery você recebe seu café onde estiver, a qualquer hora
+      </p>
+    </div>
+  )
 }
